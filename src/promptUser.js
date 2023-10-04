64b51a84427928169b4d77dc1d4d0a5b2d11e4de
Jonathan Wilferd <jwilferd10@yahoo.com>
2023-10-03 22:54:20 -0700

    Uncommented the rest of promptUser

--- src/promptUser.js
+++ src/promptUser.js
@@ -18,55 +18,55 @@ export const promptUser = () => {
             message: 'What is the title of the project? (Required)',
             validate: titleInput => titleInput ?  true : 'Please provide a title for the file!',
         },
-        // {
-        //     // Confirm file Subtitle (Optional)
-        //     type: 'confirm',
-        //     name: 'fileTitle',
-        //     message: 'Would you like to include a subtitle underneath your file name?',
-        //     default: true,
-        // },
-        // {
-        //     // Prompt subtitle iput
-        //     type: 'input',
-        //     name: 'subtitleText',
-        //     message: 'Enter your desired subtitle:',
-        //     when: ({ fileTitle }) => fileTitle,
-        // },
-        // {
-        //     // File Description (Required)
-        //     type: 'confirm',
-        //     name: 'confirmDescription',
-        //     message: 'Would you like to include a description for your file?',
-        //     default: true,
-        // },
-        // {
-        //     // User Story (Optional)
-        //     type: 'confirm', 
-        //     name: 'confirmUserStory',
-        //     message: 'Would you like to provide a User Story for this file??',
-        //     default: true,
-        // }, 
-        // {
-        //     // Resources Used (Optional)
-        //     type: 'confirm',
-        //     name: 'confirmResourcesUsed',
-        //     message: 'Would you like to highlight any resources used?',
-        //     default: true,
-        // },
-        // {
-        //     // Usage
-        //     type: 'confirm',
-        //     name: 'confirmUsage',
-        //     message: 'Would you like to include a Usage section?',
-        //     default: true,
-        // },
-        // {
-        //     // Contributors
-        //     type: 'confirm',
-        //     name: 'confirmContributors',
-        //     message: 'Would you like to include a contributors list?',
-        //     default: true,
-        // },
+        {
+            // Confirm file Subtitle (Optional)
+            type: 'confirm',
+            name: 'fileTitle',
+            message: 'Would you like to include a subtitle underneath your file name?',
+            default: true,
+        },
+        {
+            // Prompt subtitle iput
+            type: 'input',
+            name: 'subtitleText',
+            message: 'Enter your desired subtitle:',
+            when: ({ fileTitle }) => fileTitle,
+        },
+        {
+            // File Description (Required)
+            type: 'confirm',
+            name: 'confirmDescription',
+            message: 'Would you like to include a description for your file?',
+            default: true,
+        },
+        {
+            // User Story (Optional)
+            type: 'confirm', 
+            name: 'confirmUserStory',
+            message: 'Would you like to provide a User Story for this file??',
+            default: true,
+        }, 
+        {
+            // Resources Used (Optional)
+            type: 'confirm',
+            name: 'confirmResourcesUsed',
+            message: 'Would you like to highlight any resources used?',
+            default: true,
+        },
+        {
+            // Usage
+            type: 'confirm',
+            name: 'confirmUsage',
+            message: 'Would you like to include a Usage section?',
+            default: true,
+        },
+        {
+            // Contributors
+            type: 'confirm',
+            name: 'confirmContributors',
+            message: 'Would you like to include a contributors list?',
+            default: true,
+        },
         {
             // Contact Information
             type: 'confirm',
